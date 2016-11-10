f06a0cdadae279983e2102c3d49c294e319bdb68
kemblingur <eythormani@gmail.com>
2016-11-10 01:25:06 +0000

    Final

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express')
 
 var app = express();
 var port = 3000;
-
 // all environments
 app.set('port', process.env.PORT || port);
 app.set('views', __dirname + '/views');
@@ -34,23 +33,47 @@ var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function(socket){
 
-	io.sockets.emit('newUserList', {userList:users});
+	io.sockets.emit('newUserList', {users});
 
 	socket.on('addUser', function(name){
-		socket.username = name;
-		console.log(name + " has joined");
-		users.push(name);
-		io.sockets.emit('updateUserList', {user:name, connectionType:'add'});
+		socket.username = name.username;
+		socket.points = name.points;
+		var user = {};
+		console.log("HEREISAREALLYIMPORTANTTHING");
+		console.log(name);
+		user.name = name.username;
+		user.points = name.points;
+		users.push(user);
+		console.log(users);
+		io.sockets.emit('updateUserList', {user, connectionType:'add'});
 		fs.readFile(__dirname + "/lib/questions.json", "Utf-8", function(err, data){
 			socket.emit('sendQuestions', JSON.parse(data));
 		});
 	});
 	socket.on('disconnect', function(){
-		console.log(socket.username + " has disconnected");
-		var index = users.indexOf(socket.username);
+		console.log(socket.user + " has disconnected");
+		var index = users.indexOf(socket.user);
 		users.splice(index, 1);
-		io.sockets.emit('updateUserList', {user:socket.username, connectionType:'delete'});
+		io.sockets.emit('updateUserList', {user:socket.user, connectionType:'delete'});
 	});
-
-
+	console.log(users);
+	//Kóði tengdur því að uppfæra stigagjöf
+	socket.on('correctAnswer', function(data){
+		users.forEach(function(user) {
+		    if (user.name == data.username) {
+		    	user.points = data.points;
+		    	io.sockets.emit('updatePoints', {data:user});
+		    }
+		});
+	});
+	socket.on("wrongAnswer", function(data) {
+		io.sockets.emit("incorrectAnswer", {data});
+		console.log("sent to clients");
+	});
+	//code for finishing the game
+	socket.on("makeAwareOfFinishing", function(data) {
+		io.sockets.emit("sendFinishToClients", {data});
+		console.log("sent to clients");
+	});
 });
+setInterval(function(){ console.log(users) }, 1000);
